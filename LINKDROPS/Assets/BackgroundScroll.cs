90c751070ba3f071c98e1a91d7aa6be4efc3437e
thks10 <softinus@users.noreply.github.com>
2015-05-13 03:46:41 +0000

    -배경 움직임.

---
+++ LINKDROPS/Assets/BackgroundScroll.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float fVelocity = -1.0f;
+    protected Vector3 vBeginPos;
+	// Use this for initialization
+	void Start ()
+    {
+        vBeginPos = GameObject.Find("bg_002").transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        this.gameObject.transform.Translate(new Vector3(0, fVelocity, 0));
+
+        int nChildCount = this.gameObject.transform.childCount;
+
+        for(int i=0; i<nChildCount; ++i)
+        {
+            Transform tChild = this.gameObject.transform.GetChild(i);
+            if( tChild.name=="bg_001" )
+            {
+                if(tChild.position.y < -1280)
+                {
+                    tChild.position = vBeginPos;
+                }
+
+            }
+            else
+            {
+                if (tChild.position.y < -1280)
+                {
+                    tChild.position = vBeginPos;
+                }
+            }
+        }
+	}
+}
