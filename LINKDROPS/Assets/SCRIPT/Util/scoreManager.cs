aeea8e8c41bea69d38d24f097bfe2b043d61410d
mod305 <mod305@naver.com>
2015-05-22 10:05:15 +0000

    add scoreManager.cs add FONTS/joystix monospace.ttf

---
+++ LINKDROPS/Assets/SCRIPT/Util/scoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreManager : MonoBehaviour {
+
+    public GameObject managerObj;
+    public Text highscoreText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int highScore = BlockManager2.s_nScore;
+
+        highscoreText.text = "HIGHSCORE " + highScore;
+
+
+	}
+}
