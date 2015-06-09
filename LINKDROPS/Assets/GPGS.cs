83e6d80595cc66ea80104d6bf819b4c8abdbad22
thks10 <softinus@users.noreply.github.com>
2015-06-09 17:08:40 +0000

    -added: facebook, Google play game service plug-in. -update high score

---
+++ LINKDROPS/Assets/GPGS.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+
+public class GPGS : MonoBehaviour {
+
+    void OnGUI()
+    {
+        GUILayout.Label("GPGS : " + bLogined);
+    }
+
+        bool bLogined = false;
+    void Awake()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+	// Use this for initialization
+	void Start () {
+        Social.localUser.Authenticate(
+            (bool success) =>
+            {
+                bLogined = success;
+            });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
