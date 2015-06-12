5d021eeaafae3ffdf540b94df57f18a443488333
thks10 <softinus@users.noreply.github.com>
2015-06-12 21:30:56 +0000

    로그 부분 전부 삭제.

--- LINKDROPS/Assets/SCRIPT/NewScript/buttonManager.cs
+++ LINKDROPS/Assets/SCRIPT/NewScript/buttonManager.cs
@@ -44,63 +44,63 @@ public class buttonManager : MonoBehaviour {
         Social.ShowLeaderboardUI();
     }
 
-    public void Share()
-    {
-        //GameObject g1= GameObject.Find("retryButton");
-        //GameObject g2= GameObject.Find("retryButton 1");
-        //g1.active = false;
-        //g2.active = false;
-        //Application.CaptureScreenshot("test.png");
+    //public void Share()
+    //{
+    //    //GameObject g1= GameObject.Find("retryButton");
+    //    //GameObject g2= GameObject.Find("retryButton 1");
+    //    //g1.active = false;
+    //    //g2.active = false;
+    //    //Application.CaptureScreenshot("test.png");
 
-        // save picture
-        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
-        tex.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0, true);
-        tex.Apply();
-        //byte[] captureScreenShot = tex.EncodeToPNG();
-        //DestroyImmediate(tex);
-        //File.WriteAllBytes("/MyImage.png", captureScreenShot);
+    //    // save picture
+    //    Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
+    //    tex.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0, true);
+    //    tex.Apply();
+    //    //byte[] captureScreenShot = tex.EncodeToPNG();
+    //    //DestroyImmediate(tex);
+    //    //File.WriteAllBytes("/MyImage.png", captureScreenShot);
 
-        // Save your image on designate path
-        byte[] bytes = tex.EncodeToPNG();
-        string path = Application.persistentDataPath + "/MyImage.png";
-        File.WriteAllBytes(path, bytes);
+    //    // Save your image on designate path
+    //    byte[] bytes = tex.EncodeToPNG();
+    //    string path = Application.persistentDataPath + "/MyImage.png";
+    //    File.WriteAllBytes(path, bytes);
  
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+    //    AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+    //    AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
 
-        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-        intentObject.Call<AndroidJavaObject>("setType", "image/*");
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Media Sharing ");
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TITLE"), "Media Sharing ");
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Media Sharing Android Demo");
+    //    intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+    //    intentObject.Call<AndroidJavaObject>("setType", "image/*");
+    //    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Media Sharing ");
+    //    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TITLE"), "Media Sharing ");
+    //    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Media Sharing Android Demo");
  
-        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-        AndroidJavaClass fileClass = new AndroidJavaClass("java.io.File");
+    //    AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+    //    AndroidJavaClass fileClass = new AndroidJavaClass("java.io.File");
  
-        AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", path);// Set Image Path Here
-        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
+    //    AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", path);// Set Image Path Here
+    //    AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
  
-        ////	string uriPath =  uriObject.Call("getPath");
-        //bool fileExist= fileObject.Call("exists");
-        //Debug.Log("File exist : " + fileExist);
-        // Attach image to intent
-        //if (fileExist)
-        intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");       
-        currentActivity.Call("startActivity", intentObject);
-        //- See more at: http://www.theappguruz.com/unity/general-sharing-in-android-ios-in-unity/#sthash.59kcY2Lz.dpuf
+    //    ////	string uriPath =  uriObject.Call("getPath");
+    //    //bool fileExist= fileObject.Call("exists");
+    //    //Debug.Log("File exist : " + fileExist);
+    //    // Attach image to intent
+    //    //if (fileExist)
+    //    intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+    //    AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    //    AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");       
+    //    currentActivity.Call("startActivity", intentObject);
+    //    //- See more at: http://www.theappguruz.com/unity/general-sharing-in-android-ios-in-unity/#sthash.59kcY2Lz.dpuf
 
-        //AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        //AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-        //intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-        //intentObject.Call<AndroidJavaObject>("setType", "text/plain");
-        //intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
-        //intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "This is my text to send.");
-        //AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        //AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-        //currentActivity.Call("startActivity", intentObject);
-    }
+    //    //AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+    //    //AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+    //    //intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+    //    //intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+    //    //intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
+    //    //intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "This is my text to send.");
+    //    //AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    //    //AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+    //    //currentActivity.Call("startActivity", intentObject);
+    //}
 
     private string destination = "";
     private bool isProcessing = false;
@@ -112,8 +112,8 @@ public class buttonManager : MonoBehaviour {
 
     void OnGUI()
     {
-        if (isProcessing)
-            GUILayout.Label("      Saving screenshot : " + destination);
+        //if (isProcessing)
+        //    GUILayout.Label("      Saving screenshot : " + destination);
     }
 
     public IEnumerator ShareScreenshot()
