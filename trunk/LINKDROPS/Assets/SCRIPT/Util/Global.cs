16e0edeb7250978f702d427be6b6370922feee60
thks10 <thks10@01da85a6-d413-0510-8b13-92217561485b>
2015-06-16 16:15:05 +0000

    added: unity ads 5번마다 한번 씩 출력되게 함. call unity ads as soon as user dies.

---
+++ trunk/LINKDROPS/Assets/SCRIPT/Util/Global.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+
+    static public int s_nPlayCount = 0;
+    static public int s_nPlayMode = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
